2af1747974918489d852c54619b2176496f10006
Osman Emin USTA <45307514+osmanemin@users.noreply.github.com>
2021-04-12 10:31:09 +0300

    added heart and bell icon

--- component/Dashboard/Content 3/Product.js
+++ component/Dashboard/Content 3/Product.js
@@ -4,20 +4,31 @@ const Product = () => {
     return (
         <div className={style.container}>
             <div className={style.product}>
+                <div className={style.bellHeart}>
+                    <span className={style.heart}>
+
+                    </span>
+                    <span className={style.bell}>
+
+                    </span>
+                </div>
+
                 <img src="/sandalye.jpg" alt=""/>
+
                 <div className={style.detail}>
+
                     <div><b> Perilla Romee Katlanır Pratik Yeşil Kamp Sandalyesi</b></div>
                     <div className={style.price}>
                         <div>
-                            <div>n11.com</div>
-                            <div>79,90 TL</div>
+                            <div className={style.merchant}>n11.com</div>
+                            <div className={style.merchantPrice}>79,90 TL</div>
                         </div>
                         <div className={style.arrowRight}></div>
                     </div>
                     <div className={style.price}>
                         <div>
-                            <div>gittigidiyor.com</div>
-                            <div>99,90 TL</div>
+                            <div className={style.merchant}>gittigidiyor.com</div>
+                            <div className={style.merchantPrice}>99,90 TL</div>
                         </div>
                         <div className={style.arrowRight}></div>
                     </div>
